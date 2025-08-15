c03df4be468a79c76a867dee28ac355524c3bd54
Sep <septechx@proton.me>
2025-08-15 04:49:25 +0200

    feat: Add init command

--- Cli.fs
+++ Cli.fs
@@ -3,7 +3,9 @@ namespace Aqua
 module Cli =
     module Install =
         open System
+        open System.IO
         open Argu
+        open Storage
 
         type InstallArgs =
             | [<Mandatory; MainCommand>] Mods of mxm_link: string list
@@ -14,12 +16,21 @@ module Cli =
                     | Mods _ -> "mods to install"
 
         let run (args: ParseResults<InstallArgs>) =
+            if not (File.Exists(Path.Combine(getStorageDir (), "config.json"))) then
+                failwith "No config file found. Please run `aqua init` first."
+
+            let aquaConfig =
+                Path.Combine(Storage.getStorageDir (), "config.json")
+                |> Storage.loadJsonData<InstallMod.AquaConfig>
+
+            if not (File.Exists(Path.Combine(aquaConfig.game_path, "BepInEx"))) then
+                failwith "BepInEx is not installed. Please install BepInEx first."
+
             match args.TryGetResult <@ Mods @> with
             | None -> raise (ArgumentException(message = "No mods specified", paramName = "mods"))
             | Some mods ->
                 for mxm in mods do
                     InstallMod.install (mxm)
-
 
     open System
     open Argu
@@ -29,7 +40,8 @@ module Cli =
     type CmdArgs =
         | [<AltCommandLine("-v")>] Version
         | [<AltCommandLine("i"); CliPrefix(CliPrefix.None)>] Install of ParseResults<Install.InstallArgs>
-        | Validate
+        | [<CliPrefix(CliPrefix.None)>] Init
+        | [<CliPrefix(CliPrefix.None)>] Validate
 
         interface IArgParserTemplate with
             member this.Usage =
@@ -37,7 +49,7 @@ module Cli =
                 | Version -> "display version information."
                 | Validate -> "validate api key"
                 | Install _ -> "install mods"
-
+                | Init -> "initialize aqua"
 
     let private printVersion () = printfn "aqua v%s" application_version
 
@@ -64,6 +76,9 @@ module Cli =
             printfn "Validating API key..."
             let response = NexusApi.validate ()
             printfn "%s" response
+            0
+        | p when p.Contains(Init) ->
+            Init.init ()
             0
         | _ ->
             printfn "%s" (parser.PrintUsage())
---
+++ Init.fs
@@ -0,0 +1,48 @@
+namespace Aqua
+
+module Init =
+    open System
+    open System.IO
+    open System.Text.RegularExpressions
+
+    // Obtain the library path to no write a parser
+    let extractSteamPaths (vdfContent: string) =
+        let pattern = @"^\s*""path""\s+""([^""]+)"""
+
+        Regex.Matches(vdfContent, pattern, RegexOptions.Multiline)
+        |> Seq.cast<Match>
+        |> Seq.map (fun m -> m.Groups.[1].Value)
+        |> List.ofSeq
+
+
+    let private getGamePath () =
+        let libraries =
+            File.ReadAllText(
+                Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                    ".steam/root/steamapps/libraryfolders.vdf"
+                )
+            )
+            |> extractSteamPaths
+
+        // Brute force the subnautica directory
+        let gamePath =
+            libraries
+            |> List.tryFind (fun libraryPath ->
+                Path.Combine(libraryPath, "steamapps", "common", "Subnautica")
+                |> Directory.Exists)
+
+        if gamePath.IsNone then
+            failwith "Could not find Subnautica installation directory"
+
+        Path.Combine(gamePath.Value, "steamapps", "common", "Subnautica")
+
+    let private createConfigFile () =
+        let config: InstallMod.AquaConfig = { game_path = getGamePath () }
+
+        let configPath = Path.Combine(Storage.getStorageDir (), "config.json")
+        Storage.saveJsonData configPath config
+
+    let init () =
+        if not (File.Exists(Path.Combine(Storage.getStorageDir (), "config.json"))) then
+            createConfigFile ()
